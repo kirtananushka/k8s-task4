ffc8527bb9ea181e3cf9a3f69d6132f638d3e375
Kir Tananushka <kir.tananushka@verifone.com>
2023-11-12 12:36:23 +0400

    Initial commit

---
+++ .delete.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+manifests=(
+  "resource-svc-deployment.yaml"
+  "resource-svc-service.yaml"
+  "postgres-statefulset.yaml"
+  "song-svc-deployment.yaml"
+  "song-svc-service.yaml"
+  "postgres-service.yaml"
+  "config-map.yaml"
+  "db-secret.yaml"
+)
+
+for manifest in "${manifests[@]}"; do
+  echo "Deleting $manifest..."
+  kubectl delete -f "./resources/$manifest"
+done
+
+echo "Deleting namespace.yaml..."
+kubectl delete -f ./namespace/namespace.yaml
+
+echo "All manifests have been deleted."
---
+++ .run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+manifests=(
+  "db-secret.yaml"
+  "config-map.yaml"
+  "postgres-service.yaml"
+  "song-svc-service.yaml"
+  "song-svc-deployment.yaml"
+  "postgres-statefulset.yaml"
+  "resource-svc-service.yaml"
+  "resource-svc-deployment.yaml"
+)
+
+echo "Creating namespace.yaml..."
+kubectl create -f ./namespace/namespace.yaml
+
+for manifest in "${manifests[@]}"; do
+  echo "Creating $manifest..."
+  kubectl create -f "./resources/$manifest"
+done
+
+echo "All manifests have been created."
+echo
+
+NAMESPACE="ktask"
+
+# Loop until all pods are in Running status
+while true; do
+  # Get the total number of pods and the number of Running pods
+  TOTAL_PODS=$(kubectl get pods -n="$NAMESPACE" -o json | jq '.items | length')
+  RUNNING_PODS=$(kubectl get pods -n="$NAMESPACE" -o json | jq '[.items[] | select(.status.phase=="Running")] | length')
+
+  # Check if all pods are Running
+  if [[ $RUNNING_PODS -eq $TOTAL_PODS ]]; then
+    echo "All pods are running."
+    break
+  else
+    echo "Waiting for all pods to be running... ($RUNNING_PODS/$TOTAL_PODS)"
+    sleep 1
+  fi
+done
+
+echo
+
+kubectl get pods -n=ktask
